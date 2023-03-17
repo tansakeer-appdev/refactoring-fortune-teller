8af23576d314cedb96a998c3dfdf067f1d099c1d
Tan Sakeer <tansakeer@gmail.com>
2023-03-17 21:33:00 +0000

    Completed Refactoring - Dynamic Routes

---
+++ app/controllers/fortune_controller.rb
@@ -0,0 +1,22 @@
+class FortuneController < ApplicationController
+
+def horoscopes  
+
+  @sign= params.fetch("the_sign").to_sym
+
+  all_zodiacs = Zodiac.list  
+  this_zodiac = all_zodiacs.fetch(@sign)
+  @horoscope = this_zodiac.fetch(:horoscope)
+  
+  @array_of_numbers = Array.new
+
+  5.times do
+    another_number = rand(1...100)
+    
+    @array_of_numbers.push(another_number)
+  end
+
+  render({ :template => "fortunes_templates/total_fortunes.html.erb" })
+
+  end
+end
